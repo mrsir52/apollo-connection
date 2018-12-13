8bad44e02c0db5e4d6c371cbf0444a81b1b12d25
Manny <coachmannytw@gmail.com>
2018-12-13 11:59:32 -0700

    new code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>USD Currency</h1>
+  <Query
+    query={gql`
+      {
+        getPokemon(id: 1) {
+          id
+          name
+          height
+          abilities {
+            slot
+            is_hidden
+            ability {
+              name
+              url
+            }
+          }
+          stats {
+            effort
+            base_stat
+            stat {
+              name 
+              url
+            }
+          }
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      console.log("HERE ---->", data.getPokemon)
+      if (loading) return <p>Loading...</p>;
+      
+      if (error) return <p>Error :(</p>;
+      
+      
+       
+      return data.getPokemon.map( obj => {({ id }, { name }) => (
+        <div key={id}>
+          <button>{`${id}: ${name}`}</button>
+        </div>
+        
+      )});
+    }}
+  </Query>
+);
+        
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
